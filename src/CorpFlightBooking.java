64efb8c71f75d7e56b970fd83378587010dc25b0
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-07-29 15:05:46 +0530

    corporation flight booking, leetcode, medium, prefix sum

--- src/CorpFlightBooking.java
+++ src/CorpFlightBooking.java
@@ -5,16 +5,51 @@ public class CorpFlightBooking {
 //    https://leetcode.com/problems/corporate-flight-bookings/description/
 
     public static void main(String[] args) {
-        int[][] bookings = {{1, 2, 10},{
-            2, 3, 20
-        },{
-            2, 5, 25
-        }};
+        int[][] bookings = {{1, 2, 10},{2, 3, 20},{2, 5, 25}};
         int n = 5;
 
-        int[] result = corpFlightBookings(bookings, n);
+        int[] result = corpFlightBookings2(bookings, n);
 
         System.out.println(Arrays.toString(result));
+
+    }
+
+    public static int[] corpFlightBookings2(int[][] bookings, int n) {
+        int[] totalSeats = new int[n];
+
+//        System.out.println(Arrays.toString(totalSeats));
+
+
+//        prefix difference
+        for(int[] booking: bookings){
+//            {1, 2, 10} => [10,   0, -10, 0, 0]
+//            {2, 3, 20} => [10,  20, -10, -20, 0]
+//            {2, 5, 25} => [10,  45, -10, -20, 0]
+
+            int first = booking[0] -1; // shift index to 1 to 0
+            int last = booking[1] -1; // shift index to 2 to 1
+            int seats = booking[2];
+
+            totalSeats[first] += seats; // Add seats starting from 'first'
+
+            if(last + 1 < n){
+                totalSeats[last+1] -= seats; // Subtract seats after 'last'
+            }
+            System.out.println("iterations start");
+            System.out.println(Arrays.toString(totalSeats));
+            System.out.println("iterations end");
+
+
+        }
+
+//        System.out.println(Arrays.toString(totalSeats));
+
+        //   take prefix sum
+        for(int i = 1; i < n; i++){
+            totalSeats[i] += totalSeats[i-1];
+        }
+
+        return totalSeats;
 
     }
 
